e5b0277abaf5ca58d1d33b8c18be85a9e6e56b7c
d_cuzya <d.cuzya07@gmail.com>
2024-12-21 23:57:01 +0300

    a

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 import requests
 import json
+import yaml
 
-class Info:
-    nickname = None
-    player_id = None
-    percentage_of_wins = None
-    
+with open("settings.yaml", 'r') as file:
+    settings = yaml.safe_load(file)
 
-nickname = "Tvozimov"
+nickname = settings['settings']['nickname']
 headersStr = {
-    'Authorization': 'Bearer e3411b15-6b74-4ae6-8a07-043120045f85',
+    'Authorization': f'Bearer {settings['settings']['faceit_api_key']}',
 }
 
-urlStr = f"https://open.faceit.com/data/v4/players?nickname={nickname}"
-response = requests.get(url=urlStr, headers=headersStr)
-player_id = response.json()["player_id"]
-print(json.dumps(response.json(), indent=2))
-# print(player_id)
+with open('player_info.json', 'w') as file:
+    urlStr = f"https://open.faceit.com/data/v4/players?nickname={nickname}"
+    response = requests.get(url=urlStr, headers=headersStr)
+    player_id = response.json()["player_id"]
+    json.dump(response.json(),file, indent=2)
 
-# # Map statistics
-# urlStr = f"https://open.faceit.com/data/v4/players/{player_id}/stats/cs2"
-# response = requests.get(url=urlStr, headers=headersStr)
-# print(json.dumps(response.json(), indent=2))
-
-count_for_estimation = 10 
-
-urlStr = f"https://open.faceit.com/data/v4/players/{player_id}/games/cs2/stats?limit={count_for_estimation}"
-response = requests.get(url=urlStr, headers=headersStr)
-print(json.dumps(response.json(), indent=2))
+with open('stats.json', 'w') as file:
+    count_for_estimation = 10 
+    urlStr = f"https://open.faceit.com/data/v4/players/{player_id}/games/cs2/stats?limit={count_for_estimation}"
+    response = requests.get(url=urlStr, headers=headersStr)
+    json.dump(response.json(), file,indent=2)
